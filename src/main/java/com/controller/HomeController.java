09412db81a7de0b38184dc9f39c927d42ffa55d4
sockonlonton <thehungklk@gmail.com>
2020-08-24 09:46:21 +0700

    done html

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = { "/page" })
+	@RequestMapping(value = { "/page", "/page/home"})
 	public String home() {
 		return "../pages/home";
 	}
@@ -14,5 +14,30 @@ public class HomeController {
 	@RequestMapping(value = { "/page/room" })
 	public String room() {
 		return "../pages/room";
+	}
+	
+	@RequestMapping(value = { "/page/singleroom"})
+	public String singleroom() {
+		return "../pages/singleroom";
+	}
+	
+	@RequestMapping(value = { "/page/blog"})
+	public String blog() {
+		return "../pages/blog";
+	}
+	
+	@RequestMapping(value = { "/page/singleblog"})
+	public String singleblog() {
+		return "../pages/singleblog";
+	}
+	
+	@RequestMapping(value = { "/page/about"})
+	public String about() {
+		return "../pages/about";
+	}
+	
+	@RequestMapping(value = { "/page/contact"})
+	public String contact() {
+		return "../pages/contact";
 	}
 }
